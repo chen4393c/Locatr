ca4dd89ef090bb5d3a7aed4cc777188314fc35ef
chen4393c <chen4393c@gmail.com>
2018-09-14 23:49:41 -0700

    Flickr Geosearch

--- app/src/main/java/com/chen4393c/locatr/FlickrFetchr.java
+++ app/src/main/java/com/chen4393c/locatr/FlickrFetchr.java
@@ -1,5 +1,6 @@
 package com.chen4393c.locatr;
 
+import android.location.Location;
 import android.net.Uri;
 import android.util.Log;
 
@@ -69,6 +70,11 @@ public class FlickrFetchr {
         return downloadGalleryItems(url);
     }
 
+    public List<GalleryItem> searchPhotos(Location location) {
+        String url = buildUrl(location);
+        return downloadGalleryItems(url);
+    }
+
     private List<GalleryItem> downloadGalleryItems(String url) {
         List<GalleryItem> items = new ArrayList<>();
 
@@ -95,6 +101,14 @@ public class FlickrFetchr {
         }
 
         return uriBuilder.build().toString();
+    }
+
+    private String buildUrl(Location location) {
+        return ENDPOINT.buildUpon()
+                .appendQueryParameter("method", SEARCH_METHOD)
+                .appendQueryParameter("lat", "" + location.getLatitude())
+                .appendQueryParameter("lon", "" + location.getLongitude())
+                .build().toString();
     }
 
     private void parseItems(List<GalleryItem> items, JSONObject jsonBody)
